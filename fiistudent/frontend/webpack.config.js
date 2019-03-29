0b6120967c70e44741e30d283ff3bbea959cf221
Ursaciuc Adrian <ursaciuc.adrian27@gmail.com>
2019-03-29 18:35:12 +0200

    Initial project setup

---
+++ fiistudent/frontend/webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+	entry: { main: './src/index.js' },
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].[chunkhash].js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /(node_modules)/,
+				options: {
+					compact: true
+				}
+			},
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: true
+						}
+					},
+				]
+			},
+			{
+				test: /\.scss/,
+				use: [
+					'style-loader',
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: true
+						}
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							sourceMap: true,
+							includePaths: [path.join(__dirname, 'src')]
+						}
+					}
+				]
+			},
+			{
+				test: /\.(jpe?g|png|gif)$/,
+				loader: 'file-loader',
+				options: {
+					name: '[path][name].[ext]'
+				}
+			}
+		]
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			template: path.join(__dirname, 'src', 'index.html'),
+			filename: 'index.html'
+		})
+	]
+};
