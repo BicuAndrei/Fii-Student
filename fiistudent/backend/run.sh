342aec03848ff36dc162d931c2fa98ccf5c98a60
Irina Bejan <irinam.bejan@gmail.com>
2019-03-29 14:14:25 +0200

    Base project structure

---
+++ fiistudent/backend/run.sh
@@ -0,0 +1,3 @@
+echo "[i] Installing requirements..."
+pip3 install -Ur requirements.txt
+pip3 install -e .
---
+++ fiistudent/backend/setup.py
@@ -0,0 +1,13 @@
+""" Base setup script """
+
+from setuptools import setup
+
+setup(
+    name = "fiistudent",
+    version = "0.0.1",
+    description = "Yet best platform for FII Students",
+    url = "https://github.com/BicuAndrei/Fii-Student",
+    author = "Irina Bejan",
+    author_email = "irinam.bejan@gmail.com",
+)
+
