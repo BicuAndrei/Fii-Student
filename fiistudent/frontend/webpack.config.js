fde6eeb05278c7c4ecd2ef0bad8321057fafe59f
Ursaciuc Adrian <ursaciuc.adrian27@gmail.com>
2019-04-04 22:49:47 +0300

    Add landing page design

--- fiistudent/frontend/webpack.config.js
+++ fiistudent/frontend/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const FileIncludeWebpackPlugin = require('file-include-webpack-plugin')
 
 module.exports = {
 	entry: { main: './src/index.js' },
