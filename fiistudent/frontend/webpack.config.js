3cbaf78f36ac95fb359ed607a38378cf71921cf0
Ursaciuc Adrian <ursaciuc.adrian27@gmail.com>
2019-04-07 20:49:28 +0300

    Remove dev-dep

--- fiistudent/frontend/webpack.config.js
+++ fiistudent/frontend/webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const FileIncludeWebpackPlugin = require('file-include-webpack-plugin')
 
 module.exports = {
 	mode: "development",
