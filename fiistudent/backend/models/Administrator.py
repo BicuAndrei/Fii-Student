3bb5e938c1211b1b76b94eb5d4d215a4f0264e38
Dana Paduraru <38670077+danapaduraru@users.noreply.github.com>
2019-04-08 21:58:02 +0300

    refactor firstName, lastName fields

--- fiistudent/backend/models/Administrator.py
+++ fiistudent/backend/models/Administrator.py
@@ -2,7 +2,7 @@ import ndb_orm as ndb
 
 class Administrator(ndb.Model):
     id = ndb.StringProperty()
-    first_name = ndb.StringProperty()
-    last_name = ndb.StringProperty()
+    firstName = ndb.StringProperty()
+    lastName = ndb.StringProperty()
     username = ndb.StringProperty()
     password = ndb.StringProperty()
--- fiistudent/backend/models/Professor.py
+++ fiistudent/backend/models/Professor.py
@@ -2,8 +2,8 @@ import ndb_orm as ndb
 
 class Professor(ndb.Model):
     id = ndb.StringProperty()
-    first_name = ndb.StringProperty()
-    last_name = ndb.StringProperty()
+    firstName = ndb.StringProperty()
+    lastName = ndb.StringProperty()
     email = ndb.StringProperty()
     username = ndb.StringProperty()
     password = ndb.StringProperty()
