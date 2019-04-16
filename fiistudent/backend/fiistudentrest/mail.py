2dda712409b9cf3dba6e612e01ff861bc7e1ff03
Dragomas <36089404+Dragomas@users.noreply.github.com>
2019-04-17 00:07:08 +0300

    Added quick-email v1.0 and small modifications to login

--- fiistudent/backend/fiistudentrest/mail.py
+++ fiistudent/backend/fiistudentrest/mail.py
@@ -1,14 +1,14 @@
 """Sends mails via SendGrid."""
 
-
 import pkg_resources
 
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
+ENCODING = "utf-8"
 
-ENCODING="utf-8"
-DEFAULT_MAIL="irinam.bejan@gmail.com"
+
+# DEFAULT_MAIL="irinam.bejan@gmail.com"
 
 
 def get_secret(name):
@@ -16,8 +16,7 @@ def get_secret(name):
     return content.decode(ENCODING).strip()
 
 
-def send_mail(to_mails, subject, text_content, html_content=None,
-              from_mail=DEFAULT_MAIL):
+def send_mail(from_mail, to_mails, subject, text_content, html_content=None):
     html_content = html_content or text_content
     message = Mail(
         from_email=from_mail,
@@ -32,7 +31,6 @@ def send_mail(to_mails, subject, text_content, html_content=None,
     code = response.status_code
     was_successful = lambda ret_code: ret_code // 100 in (2, 3)
     if not was_successful(code):
-       raise Exception("Couldn't send e-mail: {} {}".format(code, response.body))
+        raise Exception("Couldn't send e-mail: {} {}".format(code, response.body))
 
-    print("E-mail sent from", from_mail," to ", to_mails, subject, code)
-
+    print("E-mail sent from", from_mail, " to ", to_mails, subject, code)
---
+++ fiistudent/backend/fiistudentrest/quick-mail/quick-mail.py
@@ -0,0 +1,43 @@
+import fiistudentrest.mail as Mail
+from fiistudentrest.models import Student
+from fiistudentrest.models import Professor
+import hug
+
+
+def exists(email, user_type):
+    """ Verify if the user exists in the datastore """
+    result = False
+    query = user_type.query()
+    query.add_filter('email', '=', email)
+    print(query)
+    query_it = query.fetch()
+    for ent in query_it:
+        if ent is None:
+            print('The user is not in our database.')
+            result = False
+        else:
+            print('The user exists in our database.')
+            result = True
+    return result
+
+
+@hug.local()
+@hug.get()
+@hug.cli()
+def send_email(from_email: hug.types.text, to_email: hug.types.text, subject: hug.types.text, content: hug.types.text):
+    """ Sends an email from a student to a professor """
+    if exists(from_email, Student) == True and exists(to_email, Professor) == True:
+        Mail.send_mail(from_email, to_email, subject, content)
+        return {'status': 'ok', 'errors': []}
+    elif exists(from_email, Student) == True and exists(to_email, Professor) == False:
+        return {'status': 'error',
+                'errors': [
+                    {'for': 'email_professor', 'message': 'We could not find the professor with the email({}).'.format(to_email)}]}
+    elif exists(from_email, Student) == False and exists(to_email, Professor) == True:
+        return {'status': 'error',
+                'errors': [
+                    {'for': 'email_student', 'message': 'We could not find the student with the email({}).'.format(from_email)}]}
+    else:
+        return {'status': 'error', 'errors': [
+            {'for': 'email_student', 'message': 'We could not find the student with the email({}).'.format(from_email)},
+            {'for': 'email_professor', 'message': 'We could not find the professor with the email({}).'.format(to_email)}]}
