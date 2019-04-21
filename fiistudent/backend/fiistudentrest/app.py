f3169fe1834a5f4fd661440463832395a5b49263
Irina Bejan <irinam.bejan@gmail.com>
2019-04-21 22:02:51 +0300

    Add server runner and CORS access

--- fiistudent/backend/fiistudentrest/app.py
+++ fiistudent/backend/fiistudentrest/app.py
@@ -3,5 +3,6 @@ from fiistudentrest.auth import login
 from fiistudentrest.auth import register
 
 app = hug.API(__name__)
+app.http.add_middleware(hug.middleware.CORSMiddleware(app))
 hug.get('/login', api=app)(login)
 hug.get('/register', api=app)(register)
--- fiistudent/backend/run.sh
+++ fiistudent/backend/run.sh
@@ -1,3 +1,4 @@
 echo "[i] Installing requirements..."
 pip3 install -Ur requirements.txt
 pip3 install -e .
+gunicorn fiistudentrest.app:__hug_wsgi__
