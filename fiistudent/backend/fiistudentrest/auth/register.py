679429e0a73dd26e1918c58fd1b53d67733d3238
Dragomas <36089404+Dragomas@users.noreply.github.com>
2019-05-07 16:44:30 +0300

    Added phone number to register function, modified Student model

--- fiistudent/backend/fiistudentrest/auth/register.py
+++ fiistudent/backend/fiistudentrest/auth/register.py
@@ -1,6 +1,7 @@
 from google.cloud import datastore
 from fiistudentrest.models import Student
 import hug
+import phonenumbers
 
 
 def entity_exists(entity):
@@ -30,27 +31,48 @@ def register_function(entity):
     return False
 
 
+def is_real_number(phone_number):
+    """ Verifies if a phone number is valid for Romania """
+    the_phone_number = phonenumbers.parse(phone_number, "RO")
+    return phonenumbers.is_valid_number_for_region(the_phone_number, "RO")
+
+
 @hug.get()
 @hug.cli()
 @hug.local()
 def register(registrationNumber: hug.types.text, firstName: hug.types.text, lastName: hug.types.text,
-             email: hug.types.text, username: hug.types.text, password: hug.types.text,
+             email: hug.types.text, phone_number: hug.types.text, password: hug.types.text,
              confirm_password: hug.types.text, year: hug.types.number,
-             group: hug.types.text, hug_timer=3):
+             group: hug.types.text):
     """ Adds the entity in the datastore if possible and return an appropriate json response for every scenario """
-    # verify if the password and confirm password match, if they don't match, return a json response
-    if password != confirm_password:
+
+    if len(firstName) < 3:
         return {'status': 'error',
-                'errors': [{'for': 'confirm_password', 'message': "The password and the confirm password don't match"}]}
+                'errors': [
+                    {'for': 'firstName', 'message': "The first name is not 3+ letters long."}]}
+    elif len(lastName) < 3:
+        return {'status': 'error',
+                'errors': [
+                    {'for': 'lastName', 'message': "The last name is not 3+ letters long."}]}
+    elif len(password) < 6:
+        return {'status': 'error',
+                'errors': [
+                    {'for': 'password', 'message': "The password is not 6+ characters long."}]}
+    elif password != confirm_password:
+        return {'status': 'error',
+                'errors': [
+                    {'for': 'confirm_password', 'message': "The password and the confirm password don't match."}]}
+    elif is_real_number(phone_number) == False:
+        return {'status': 'error',
+                'errors': [{'for': 'phoneNumber', 'message': "The number is not valid."}]}
     else:
         # create the entity
-
         student = Student(
             registrationNumber=registrationNumber,
-            firstName=firstName,
-            lastName=lastName,
+            firstName=firstName.capitalize(),
+            lastName=lastName.capitalize(),
             email=email.lower(),
-            username=username,
+            phoneNumber=phone_number,
             password=password,
             year=year,
             group=group.upper()
