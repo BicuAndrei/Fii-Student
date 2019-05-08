a174068edc0165659e2738b6df5094c932280065
Dana Paduraru <38670077+danapaduraru@users.noreply.github.com>
2019-05-08 10:14:06 +0300

    compare feedback week number

--- fiistudent/backend/fiistudentrest/feedback_functionality/submit_feedback.py
+++ fiistudent/backend/fiistudentrest/feedback_functionality/submit_feedback.py
@@ -1,5 +1,9 @@
 import hug
 import json
+import datetime
+# import os
+
+# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:\\Users\\Dana\\Documents\\fii-student-5c3ee6b54bbd.json"
 
 from fiistudentrest.models import Student
 from fiistudentrest.models.feedback import Feedback
@@ -9,8 +13,16 @@ from fiistudentrest.auth import verify_token
 @hug.local()
 @hug.get()
 @hug.cli()
-def submit_feedback(request, professor: hug.types.number, date: hug.types.text, stars: hug.types.number,
-                    feedback: hug.types.text):
+def submit_feedback(request, professor: hug.types.text, course: hug.types.text, date: hug.types.text,
+                    stars: hug.types.number, feedback: hug.types.text):
+    """
+    professor = "5105779283591168"
+    course = "6476246011609088"
+    date = "2019-05-03 (14:52:39.712) EEST"
+    stars = 5
+    feedback = "Mi-a placut"
+    student = Student.get("ag1lfmZpaS1zdHVkZW50chQLEgdTdHVkZW50GICAgNjGgocJDKIBC2RldmVsb3BtZW50")
+    """
     authorization = request.get_header('Authorization')
     if not authorization:
         return {'status': 'error',
@@ -27,21 +39,23 @@ def submit_feedback(request, professor: hug.types.number, date: hug.types.text, 
     data_list = []
     data = {}
     # check if feedback already exists
-    entity_feedback = feedback_exists(professor, student, date)
+    entity_feedback = feedback_exists(professor, student, date, course)
     if entity_feedback is not None:
         data["feedbackSubmitted"] = True
+        data["course"] = entity_feedback.course
         data["stars"] = entity_feedback.stars
         data["feedback"] = entity_feedback.text
     else:
         data["feedbackSubmitted"] = False
         data["profId"] = professor
+        data["course"] = course
         data["stars"] = stars
         data["feedback"] = feedback
         # add feedback to db
         feedback_db = Feedback(
             student=student,
             professor=professor,
-            # course=feedback.course, ?????
+            course=course,
             text=feedback,
             stars=stars,
         )
@@ -52,17 +66,19 @@ def submit_feedback(request, professor: hug.types.number, date: hug.types.text, 
     return json_data
 
 
-def feedback_exists(professor, student, date):
+def feedback_exists(professor, student, date, course):
     # checks if feedback exists from a user to a professor
     query = Feedback.query()
     query.add_filter('professor', '=', professor)
     query.add_filter('student', '=', student)
-    query.add_filter('created_at', '=', date)
+    query.add_filter('course', '=', course)
+    feedback_week = get_week(date)
     query_it = query.fetch()
     for ent in query_it:
+        existing_feedback_week = get_week(ent.created_at)
         if ent is None:
             print('The professor has not received any feedback from this user at this date.')
-        else:
+        elif existing_feedback_week == feedback_week:
             print('The user has submitted feedback for this professor at this date.')
             return ent
     return None
@@ -74,7 +90,7 @@ def get_all_feedback():
     feedback_query_it = feedback_query.fetch()
     data_list = []
     for ent in feedback_query_it:
-        data = {'stars': ent.stars, 'feedback': ent.text, 'date': ent.created_at}
+        data = {'stars': ent.stars, 'feedback': ent.text, 'date': ent.created_at, 'course': ent.course}
         data_list.append(data)
 
     json_data = json.dumps(data_list)
@@ -88,7 +104,7 @@ def get_feedback_by_professor(professor):
     query_it = query.fetch()
     data_list = []
     for ent in query_it:
-        data = {'stars': ent.stars, 'feedback': ent.text, 'date': ent.created_at}
+        data = {'stars': ent.stars, 'feedback': ent.text, 'date': ent.created_at, 'course': ent.course}
         data_list.append(data)
 
     json_data = json.dumps(data_list)
@@ -104,11 +120,19 @@ def get_feedback_by_professor_date(professor, start_date, end_date):
     query_it = query.fetch()
     data_list = []
     for ent in query_it:
-        data = {'stars': ent.stars, 'feedback': ent.text, 'date': ent.created_at}
+        data = {'stars': ent.stars, 'feedback': ent.text, 'date': ent.created_at, 'course': ent.course}
         data_list.append(data)
 
     json_data = json.dumps(data_list)
     return json_data
+
+
+def get_week(date):
+    # returns the number of the week for a given date
+    # standard RFC 3339 for datastore is: '%Y-%m-%dT%H:%M:%S.%fZ'
+    feedback_date = datetime.datetime.strptime(date,
+                                               '%Y-%m-%d (%H:%M:%S.%f) EEST')
+    return datetime.date(feedback_date.year, feedback_date.month, feedback_date.day).isocalendar()[1]
 
 
 if __name__ == '__main__':
