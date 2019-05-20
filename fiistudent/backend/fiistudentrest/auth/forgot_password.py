51a346650dbdc64defaf7ccbf932d72e00d3977b
Dragomas <36089404+Dragomas@users.noreply.github.com>
2019-05-20 08:44:14 +0300

    Forgot-password functionality

---
+++ fiistudent/backend/fiistudentrest/auth/forgot_password.py
@@ -0,0 +1,76 @@
+from fiistudentrest.models import Student
+from itsdangerous import URLSafeSerializer, SignatureExpired
+import fiistudentrest.mail as Mail
+import hug
+
+
+def entity_exists(email):
+    """ Verify if the entity exists in the datastore """
+    result = False
+    query = Student.query()
+    query.add_filter('email', '=', email)
+    query_it = query.fetch()
+    for ent in query_it:
+        if ent is None:
+            result = False
+        else:
+            result = True
+    return result
+
+
+def generate_token(email, salt='forgot-password'):
+    serializer = URLSafeSerializer("Thisisasecret!!!")
+    return serializer.dumps(email, salt)
+
+
+def confirm_token(token):
+    serializer = URLSafeSerializer("Thisisasecret!!!")
+    try:
+        email = serializer.loads(token, salt='forgot-password')
+    except SignatureExpired:
+        return False
+    return email
+
+
+def send_forgot_password_email(to_email):
+    link = 'endpoint'
+    token = generate_token(to_email)
+    link_token = link + token
+
+    subject = 'Change your password'
+    content = '<body><h4>Hi there,</h4>' \
+              '<p>This message is sent because you requested a change of password.</p>' \
+              '<div style="display:flex; flex-direction: column; align-items: center">' \
+              '<p>To change your password click on the button below.</p>' \
+              '<a href="' + link_token + '" style="border: none;cursor: pointer;padding: 10px 20px;' \
+                                         'border-radius: 5px;font-size: 15px; background-color: #21d146;' \
+                                         'font-weight: bold; text-decoration:none; color:black">' \
+                                         'Change your password</button>' \
+                                         '</div></body>'
+    Mail.send_mail(Mail.DEFAULT_MAIL, to_email, subject, content)
+
+
+# @hug.get()
+# @hug.cli()
+# @hug.local()
+def forgot_password(email: hug.types.text):
+    if entity_exists(email) == True:
+        send_forgot_password_email(email)
+        return {'status': 'ok'}
+    else:
+        return {'status': 'error', 'errors': [
+            {'for': 'forgot_password', 'message': 'This email does not exist in our database'}]}
+
+
+@hug.get()
+@hug.cli()
+@hug.local()
+def confirm_forgot_password_token(token):
+    if confirm_token(token) != False:
+        return {'status': 'ok'}
+    else:
+        return {'status': 'error', 'errors': [
+            {'for': 'forgot_password', 'message': 'The link is invalid or has expired'}]}
+
+if __name__ == '__main__':
+    forgot_password('dragomircristian323@yahoo.com')
